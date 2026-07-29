54d790f97e058c7bca7dec52786d9fbdbd994bfd
ranjith <ranjithsnair@live.com>
2026-07-29 16:36:31 +0530

    Initial check-in

--- kernel/include/keyboard.h
+++
@@ -1,9 +0,0 @@
-/* PS/2 keyboard (port 0x60): decodes keypresses and feeds them to
- * console.c's line discipline. See keyboard.c for the scancode table. */
-#ifndef KEYBOARD_H
-#define KEYBOARD_H
-
-/* Registers the IRQ1 handler and unmasks it at the PIC. Call once at boot. */
-void keyboard_init(void);
-
-#endif
